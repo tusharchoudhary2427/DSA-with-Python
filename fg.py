85e7faf2904d6eaa680cb7fe91301411c95e60f7
tusharchoudhary2427 <tushar.choudhary2427@gmail.com>
2025-08-07 11:16:41 +0530

    Added something

---
+++ fg.py
@@ -0,0 +1,70 @@
+# for smallest and second smallest
+
+nums = [5,-8,22,19,3,27,-37]
+n = len(nums)
+
+if nums[0] > nums[1]:
+    smallest = nums[1]
+    second_smallest = nums[0]
+
+else:
+    smallest = nums[0]
+    second_smallest = nums[1]
+
+for i in range (2,n):
+    if nums[i] < second_smallest:
+
+        if nums[i] < smallest:
+            second_smallest = smallest
+            smallest = nums[i]
+        
+        else:
+            second_smallest = nums[i]
+        
+
+print(smallest, second_smallest)
+
+# largest and second largest
+
+nums = [5,-8,22,19,3,27,-37]
+n = len(nums)
+
+if nums[0] < nums[1]:
+    largest = nums[1]
+    second_largest = [0]
+
+else:
+    largest = nums[0]
+    second_largest = nums[1]
+
+print(largest,second_largest)
+
+for i in range(2,n):
+    if nums[i] > second_largest:
+        if nums[i] > largest:
+            second_largest = largest 
+            largest = nums[i]
+        else:
+            second_largest = nums[i]
+
+print(second_largest,largest)
+
+
+
+
+
+
+
+        
+    
+
+
+
+
+
+
+       
+
+
+
+
